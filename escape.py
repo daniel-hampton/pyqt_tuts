bad0077705731b9eda8e312d65f4b5750d8bcf6d
Dan Hampton <dhampton084@gmail.com>
2018-06-26 12:34:41 -0400

    Started work on Signals and slots. On escape.py currently.

---
+++ escape.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        pass
